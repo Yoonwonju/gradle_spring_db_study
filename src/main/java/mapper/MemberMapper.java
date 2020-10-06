a2279e14fc56ccbe9319d1d3e56aeb248a9ec8cc
yoon <lrzrlove@naver.com>
2020-10-06 16:40:19 +0900

    Complete

---
+++ src/main/java/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package mapper;
+
+import java.util.List;
+
+import gradle_spring_db_study.spring.Member;
+
+public interface MemberMapper {
+    List<Member> selectAll();
+    int insert(Member member);
+}
